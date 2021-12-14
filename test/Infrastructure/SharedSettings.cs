2ab6437c9eca2e95a7842cb0387673b7017f7be1
Dan <daningalla@gmail.com>
2021-12-14 13:26:02 -0700

    Add scrubbers to Verified tests (compensate for compiler generated method sigs)

--- test/Infrastructure/SharedSettings.cs
+++ test/Infrastructure/SharedSettings.cs
@@ -1,15 +1,20 @@
-﻿using VerifyTests;
+﻿using System.Text.RegularExpressions;
+using VerifyTests;
 
 namespace Vertical.SpectreLogger.Tests.Infrastructure
 {
     public static class SharedSettings
     {
-        public static readonly VerifySettings Verifier = Factory.New<VerifySettings>(() =>
+        public static readonly VerifySettings Verifier = Factory.New(() =>
         {
             var settings = new VerifySettings();
             
             settings.UniqueForRuntime();
             settings.UseDirectory("Verified");
+            settings.ScrubLinesWithReplace(src => Regex.Replace(
+                src,
+                @"b__\d+_\d+\(",
+                "b__ANY("));
             
             return settings;
         });
