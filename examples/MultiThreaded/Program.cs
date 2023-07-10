ce5f28790f77c93e1fc37eafde0cb86166c5652a
Dan <daningalla@gmail.com>
2023-07-09 23:02:40 -0600

    Fixes #25, make OptionsCollection use ConcurrentDictionary

---
+++ examples/MultiThreaded/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Vertical.SpectreLogger;
+
+var loggerFactory = LoggerFactory.Create(builder => builder.AddSpectreConsole());
+var logger = loggerFactory.CreateLogger("Test");
+var threads = Enumerable.Range(0, 25).Select(i =>
+    Task.Run(() =>
+    {
+        for (var c = 0; c < 100; c++)
+        {
+            if (c % 25 == 0)
+            {
+                try
+                {
+                    Throw();
+                }
+                catch (InvalidOperationException exception)
+                {
+                    logger.LogError(exception, "An InvalidOperationException was thrown");
+                }
+                continue;
+            }
+
+            logger.LogInformation("I'm thread {thread}, and I am on iteration {iteration}",
+                i,
+                c);
+        }
+        return Task.CompletedTask;
+    }));
+
+await Task.WhenAll(threads);
+
+static void Throw()
+{
+    throw new InvalidOperationException();
+}
