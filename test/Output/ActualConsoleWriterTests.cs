422e3ab43cf9efdc04c8ec315a22f8458a68ca83
Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>
2023-08-24 09:54:16 +0100

    Write Error tests

--- test/Output/ActualConsoleWriterTests.cs
+++ test/Output/ActualConsoleWriterTests.cs
@@ -1,11 +1,7 @@
-﻿using System.Text;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NSubstitute;
-using Spectre.Console;
-using Spectre.Console.Rendering;
-using Vertical.SpectreLogger.Output;
 using Xunit;
 
 namespace Vertical.SpectreLogger.Tests.Output
@@ -44,6 +40,42 @@ namespace Vertical.SpectreLogger.Tests.Output
             var logger = services.GetRequiredService<ILogger<ActualConsoleWriterTests>>();
             
             logger.LogInformation("Test event successful");
+
+            await services.DisposeAsync();
+        }
+        
+        [Fact]
+        public async Task WriteErrorInBackground()
+        {
+            var services = new ServiceCollection()
+                .AddLogging(builder => builder.AddSpectreConsole(
+                    config =>
+                    {
+                        config.WriteInBackground();
+                    }))
+                .BuildServiceProvider();
+
+            var logger = services.GetRequiredService<ILogger<ActualConsoleWriterTests>>();
+
+            logger.LogError(new ArgumentException(), "Oops");
+
+            await services.DisposeAsync();
+        }
+        
+        [Fact]
+        public async Task WriteErrorInForeground()
+        {
+            var services = new ServiceCollection()
+                .AddLogging(builder => builder.AddSpectreConsole(
+                    config =>
+                    {
+                        config.WriteInForeground();
+                    }))
+                .BuildServiceProvider();
+
+            var logger = services.GetRequiredService<ILogger<ActualConsoleWriterTests>>();
+            
+            logger.LogError(new ArgumentException(), "Oops");
 
             await services.DisposeAsync();
         }
