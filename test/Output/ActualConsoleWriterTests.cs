54678022b573264b2889cda745fff743f0fb5f25
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-08-24 13:50:32 +0100

    Fix Invalid Console Handle (#28)
    * Add Windows Dev Build
    
    * Trigger on pull request
    
    * Trigger on this branch?
    
    * ActualConsoleWriterTests
    
    * if (!Console.IsOutputRedirected)
    
    * Write Error tests
    
    * Remove custom branch from pipeline file
    
    * Change name in yaml
    
    ---------
    
    Co-authored-by: Tom Longhurst <130671542+msm-tomlonghurst@users.noreply.github.com>

---
+++ test/Output/ActualConsoleWriterTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace Vertical.SpectreLogger.Tests.Output
+{
+    public class ActualConsoleWriterTests
+    {
+        [Fact]
+        public async Task WriteInBackground()
+        {
+            var services = new ServiceCollection()
+                .AddLogging(builder => builder.AddSpectreConsole(
+                    config =>
+                    {
+                        config.WriteInBackground();
+                    }))
+                .BuildServiceProvider();
+
+            var logger = services.GetRequiredService<ILogger<ActualConsoleWriterTests>>();
+            
+            logger.LogInformation("Test event successful");
+
+            await services.DisposeAsync();
+        }
+        
+        [Fact]
+        public async Task WriteInForeground()
+        {
+            var services = new ServiceCollection()
+                .AddLogging(builder => builder.AddSpectreConsole(
+                    config =>
+                    {
+                        config.WriteInForeground();
+                    }))
+                .BuildServiceProvider();
+
+            var logger = services.GetRequiredService<ILogger<ActualConsoleWriterTests>>();
+            
+            logger.LogInformation("Test event successful");
+
+            await services.DisposeAsync();
+        }
+        
+        [Fact]
+        public async Task WriteErrorInBackground()
+        {
+            var services = new ServiceCollection()
+                .AddLogging(builder => builder.AddSpectreConsole(
+                    config =>
+                    {
+                        config.WriteInBackground();
+                    }))
+                .BuildServiceProvider();
+
+            var logger = services.GetRequiredService<ILogger<ActualConsoleWriterTests>>();
+
+            logger.LogError(new ArgumentException(), "Oops");
+
+            await services.DisposeAsync();
+        }
+        
+        [Fact]
+        public async Task WriteErrorInForeground()
+        {
+            var services = new ServiceCollection()
+                .AddLogging(builder => builder.AddSpectreConsole(
+                    config =>
+                    {
+                        config.WriteInForeground();
+                    }))
+                .BuildServiceProvider();
+
+            var logger = services.GetRequiredService<ILogger<ActualConsoleWriterTests>>();
+            
+            logger.LogError(new ArgumentException(), "Oops");
+
+            await services.DisposeAsync();
+        }
+    }
+}
