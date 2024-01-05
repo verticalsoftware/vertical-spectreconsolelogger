5afdc5af300ca264caa27d84280ebd65bcdbbf8f
Dan <daningalla@gmail.com>
2024-01-04 21:21:07 -0700

    example

---
+++ examples/CustomConsoleWriter/NewLineReplacingWriter.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+using Vertical.SpectreLogger.Output;
+
+namespace CustomConsoleWriter;
+
+public class NewLineReplacingWriter : ConsoleWriter, IConsoleWriter
+{
+    public NewLineReplacingWriter(IAnsiConsole console) : base(console)
+    {
+    }
+
+    /// <inheritdoc />
+    public void ResetLine() => ResetLineCore();
+
+    /// <inheritdoc />
+    public void Write(string content)
+    {
+        var replaced = content.Replace(
+            Environment.NewLine,
+            "\u2028");
+        
+        WriteToConsole(replaced);
+    }
+}
---
+++ examples/CustomConsoleWriter/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CustomConsoleWriter;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Vertical.SpectreLogger;
+using Vertical.SpectreLogger.Output;
+
+var factory = LoggerFactory.Create(builder =>
+{
+    builder.AddSpectreConsole();
+    builder.Services.AddSingleton<IConsoleWriter, NewLineReplacingWriter>();
+});
+
+var logger = factory.CreateLogger<Program>();
+
+logger.LogInformation(
+    "This string used to contain a new line character," +
+    Environment.NewLine +
+    "but now it is removed");
+    
+    
